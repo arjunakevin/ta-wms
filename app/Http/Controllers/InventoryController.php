385c1f54f321fe64c5395b0b7035ebdc2867aa36
Kevin Arjuna <arjunakevin48@gmail.com>
2021-10-20 15:14:16 +0700

    Group inventory

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Inventory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InventoryController extends Controller
 {
@@ -14,7 +15,24 @@ class InventoryController extends Controller
      */
     public function index()
     {
-        $data = Inventory::with('product', 'location')->paginate();
+        $data = Inventory::with('product', 'location')
+            ->select(
+                'inventories.id',
+                'inventories.location_id',
+                'inventories.documentable_type',
+                'inventories.documentable_id',
+                'inventories.detail_id',
+                'inventories.product_id',
+                DB::raw('SUM(base_quantity) base_quantity'),
+                DB::raw('SUM(put_quantity) put_quantity'),
+                DB::raw('SUM(pick_quantity) pick_quantity')
+            )
+            ->groupBy(
+                'product_id',
+                'location_id',
+                'documentable_id'
+            )
+            ->paginate();
 
         return inertia()->render('Inventory/Index', compact('data'));
     }

9a65f96588c27b44d74e590f666a4a80e344e112
Kevin Arjuna <arjunakevin48@gmail.com>
2021-10-26 16:49:51 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createInertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaProgress } from '@inertiajs/progress';
 import Layout from './Pages/Layouts/Main';
 import VueTypeaheadBootstrap from 'vue-typeahead-bootstrap';
+import Select2 from './Components/Select2'
 
 InertiaProgress.init();
 
@@ -12,12 +13,23 @@ Vue.prototype.$axios = axios;
 Vue.prototype.$route = route;
 
 Vue.component('vue-typeahead-bootstrap', VueTypeaheadBootstrap);
+Vue.component('select2', Select2);
 
 const getInboundStatus = status => {
     const label  = {
         '1': 'Unreceived',
         '2': 'Partially Received',
         '-1': 'Fully Received'
+    };
+
+    return label[status];
+}
+
+const getOutboundStatus = status => {
+    const label  = {
+        '1': 'Uncommitted',
+        '2': 'Partially Committed',
+        '-1': 'Fully Committed'
     };
 
     return label[status];
@@ -31,6 +43,19 @@ const getGrStatus = status => {
         '4': 'Received',
         '5': 'Partial Putaway',
         '-1': 'Full Putaway',
+    };
+
+    return label[status];
+}
+
+const getDoStatus = status => {
+    const label = {
+        '1': 'Uncommitted',
+        '2': 'Partial Pick',
+        '3': 'Full Pick',
+        '4': 'Partial Check',
+        '5': 'Full Check',
+        '-1': 'Good Issue'
     };
 
     return label[status];
@@ -69,6 +94,28 @@ Vue.mixin({
             
             return `<span class="badge ${grState}">${grLabel}</span>`;
         },
+        getOutboundStatusLabel: status => {
+            const state = status > 0 ? 'badge-primary' : 'badge-success';
+            let label = getOutboundStatus(status);
+            
+            return `<span class="badge ${state}">${label}</span>`;
+        },
+        getDoStatusLabel: status => {
+            const state = {
+                '1': 'badge-primary',
+                '2': 'badge-warning',
+                '3': 'badge-info',
+                '4': 'badge-warning',
+                '5': 'badge-info',
+                '-1': 'badge-success'
+            };
+
+            const doState = state[status];
+            const doLabel = getDoStatus(status);
+            
+            return `<span class="badge ${doState}">${doLabel}</span>`;
+        },
+        getOutboundStatus: status => getOutboundStatus(status),
         getMovementStatusLabel: status => {
             const state = {
                 '0': 'badge-primary',
@@ -87,7 +134,8 @@ Vue.mixin({
             
             return `<span class="badge ${movementState}">${movementLabel}</span>`;
         },
-        getGrStatus: status => getGrStatus(status)
+        getGrStatus: status => getGrStatus(status),
+        getDoStatus: status => getDoStatus(status)
     }
 })
 

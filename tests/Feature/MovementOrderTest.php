3b7aa9b2bff8b21da908528a80f57708e6e199b1
Kevin Arjuna <arjunakevin48@gmail.com>
2021-10-27 09:57:59 +0700

    Update movement order test

--- tests/Feature/MovementOrderTest.php
+++ tests/Feature/MovementOrderTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\Base;
+use App\Models\Location;
 use App\Models\Inventory;
 use App\Models\GoodReceive;
 use Inertia\Testing\Assert;
@@ -14,7 +15,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class MovementOrderTest extends Base
 {
     use RefreshDatabase;
-    
+
     /** @test */
     public function can_visit_movement_order_list_page()
     {
@@ -77,6 +78,33 @@ class MovementOrderTest extends Base
     }
 
     /** @test */
+    public function can_add_movement_detail()
+    {
+        // $this->withoutExceptionHandling();
+        $good_receive = GoodReceive::factory()
+            ->hasInventories(['base_quantity' => 100, 'pick_quantity' => 0])
+            ->has(MovementOrder::factory(), 'movement_orders')
+            ->create();
+
+        $inventory = $good_receive->inventories->first();
+        $movement = $good_receive->movement_orders->first();
+
+        $data = [
+            'product_code' => $inventory->product->code,
+            'base_quantity' => $inventory->base_quantity,
+            'location_code' => Location::factory(['put_blocked' => 0])->create()->code
+        ];
+
+        $this->assertDatabaseCount('inventories', 1);
+        $this->assertCount(0, $movement->details);
+
+        $res = $this->post(route('movement_order_details.store', $movement), $data);
+
+        $this->assertDatabaseCount('inventories', 2);
+        $this->assertCount(1, $movement->fresh()->details);
+    }
+
+    /** @test */
     public function can_confirm_movement_order()
     {
         $this->withoutExceptionHandling();
@@ -113,7 +141,7 @@ class MovementOrderTest extends Base
     }
 
     /** @test */
-    public function pick_movement_order()
+    public function can_cancel_movement_order()
     {
         $movement_order = MovementOrderDetail::factory()->create();
         $source_inventory = $movement_order->source_inventory;
@@ -145,5 +173,11 @@ class MovementOrderTest extends Base
         $this->assertEquals($source_base_quantity, $source_inventory->base_quantity);
         $this->assertEquals($destination_base_quantity, $destination_inventory->base_quantity);
         $this->assertEquals($destination_put_quantity - $base_quantity, $destination_inventory->put_quantity);
+    }
+
+    /** @test */
+    public function can_print_movement_list()
+    {
+        $this->assertTrue(true);
     }
 }
